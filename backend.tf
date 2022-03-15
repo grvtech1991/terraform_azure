3c8a8b90678a41445b40eebe0cbd1ae9f80d6d78
grvtech1991 <37359952+grvtech1991@users.noreply.github.com>
2022-03-16 02:12:43 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "terraform1991"
+    bucket = "terraform-1991"
     key = "terraform.tfstate"
-    region = "us-east-1"
+    region = "ap-south-1"
     dynamodb_table = "terraform"
   }
 }
